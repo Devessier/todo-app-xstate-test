ff59378e64e839bf7f19781667966e46aa7c240e
Devessier <baptiste@devessier.fr>
2021-09-10 19:59:40 +0200

    build: configure TailwindCSS with JIT mode

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './*.html',
+    './src/**/*.{ts,vue}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
