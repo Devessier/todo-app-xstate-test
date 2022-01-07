0d9c6447473a8435cb16a8d498abfd8cfc41964d
Devessier <baptiste@devessier.fr>
2022-01-08 00:16:28 +0100

    build: configure vitest to replace jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  moduleFileExtensions: ["js", "ts", "json", "vue"],
-  transform: {
-    "^.+\\.ts$": "ts-jest",
-    "^.+\\.vue$": "@vue/vue3-jest",
-  },
-  setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
-};
